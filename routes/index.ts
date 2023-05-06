b1639277d7a8f05a31d7ab4b619b3594d5985898
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2023-05-07 00:05:45 +0530

    add: handle multipart/form-data using  multer

--- routes/index.ts
+++ routes/index.ts
@@ -8,6 +8,27 @@ import {
 } from "../controllers";
 import { authMiddleware } from "../middlewares";
 
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const suffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    const uniqueName = `${file.originalname}-${suffix}${path.extname(
+      file.originalname
+    )}`;
+    cb(null, uniqueName);
+  },
+});
+
+let upload = multer({
+  storage,
+  limits: { fileSize: 10 * 1024 * 1024 }, // 5MB
+});
+
 // register
 router.post("/auth/register", registerController.register);
 // session
@@ -16,6 +37,6 @@ router.post("/logout", authMiddleware, sessionController.logout);
 // refresh
 router.post("/refresh", authMiddleware, refreshController.refersh);
 // product
-router.post("/upload/product", productController.add);
+router.post("/upload/product", upload.single("image"), productController.add);
 
 export default router;
